dff404dae758898adfd651868f5a337cdf359215
AlessioMura <alessiomura03@gmail.com>
2024-10-17 18:30:19 +0200

    added function to get emails

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
 //generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+//Prep
+const email_list = document.getElementById('email_list');
+
+//Processing
+function get_email() {
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(response => {
+        const email = response.data.response;
+        const item_list = document.createElement('li');
+        item_list.textContent = email;
+        email_list.appendChild(item_list);
+    })
+}
